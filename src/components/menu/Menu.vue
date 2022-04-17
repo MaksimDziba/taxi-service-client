d92965b57f0b5e24c1bee127ba20867f80e9c444
maksim dziba <mdziba@mail.ru>
2022-04-17 11:08:44 +0300

    udpate main menu

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,30 +1,36 @@
 <template>
   <a-menu theme="dark" v-model:selectedKeys="selectedKeys" mode="inline">
     <a-menu-item key="1">
+      <appstore-outlined />
+      <span class="menu-item">
+        <router-link to="/">Главная</router-link>
+      </span>
+    </a-menu-item>
+    <a-menu-item key="2">
       <user-outlined />
       <span class="menu-item">
         <router-link to="/drivers">Водители</router-link>
       </span>
     </a-menu-item>
-    <a-menu-item key="2">
+    <a-menu-item key="3">
       <alert-outlined />
       <span class="menu-item">
         <router-link to="/vehicles">Транспорт</router-link>
       </span>
     </a-menu-item>
-    <a-menu-item key="3">
+    <a-menu-item key="4">
       <thunderbolt-outlined />
       <span class="menu-item">
         <router-link to="/orders">Заказы</router-link>
       </span>
     </a-menu-item>
-    <a-menu-item key="4">
+    <a-menu-item key="5">
       <usergroup-add-outlined />
       <span class="menu-item">
         <router-link to="/clients">Клиенты</router-link>
       </span>
     </a-menu-item>
-    <a-menu-item key="5">
+    <a-menu-item key="6">
       <safety-certificate-outlined />
       <span class="menu-item">
         <router-link to="/tariffs">Тарифы</router-link>
@@ -34,17 +40,23 @@
 </template>
 
 <script lang="ts">
+import { defineComponent, ref } from "vue";
+
+// icons
 import {
+  AppstoreOutlined,
   UserOutlined,
   AlertOutlined,
   ThunderboltOutlined,
   UsergroupAddOutlined,
   SafetyCertificateOutlined,
 } from "@ant-design/icons-vue";
-import { defineComponent, ref } from "vue";
+
+const items = ["", "/", "/drivers", "/vehicles", "/orders", "/clients"];
 
 export default defineComponent({
   components: {
+    AppstoreOutlined,
     UserOutlined,
     AlertOutlined,
     ThunderboltOutlined,
@@ -52,13 +64,17 @@ export default defineComponent({
     SafetyCertificateOutlined,
   },
   data() {
+    const routerPath = window.location.pathname;
+    const current = items.findIndex((item) => item === routerPath) || "1";
+
     return {
       collapsed: ref<boolean>(false),
-      selectedKeys: ref<string[]>(["1"]),
+      selectedKeys: ref<string[]>([`${current}`]),
     };
   },
 });
 </script>
+
 <style>
 #components-layout-demo-side .logo {
   height: 32px;
